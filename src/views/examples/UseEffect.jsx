5443fc3352dd6eecd69e6a430b7f3abd315a2bd2
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-19 12:20:26 -0300

    s5a64: useEffect #01

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+function calcFatorial(num){
+    const n = parseInt(num)
+    if (n < 0) return -1;
+    if (n === 0) return 1;
+    return calcFatorial(n -1) * n   
+}
 
 const UseEffect = (props) => {
+    const [number, setNumber] = useState(1)
+    const [fatorial, setFatorial] = useState(1)
+
+    useEffect(function(){
+        setFatorial(calcFatorial(number))
+    }, [number])
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <SectionTitle title="Exercicio #01"/>
+            <div className="center">
+                <div>
+                    <span className="text">
+                        Fatorial:
+                    </span>
+                    <span className="text red">
+                        {fatorial === -1 ? 'Nao Existe' :  fatorial}
+                    </span>
+                </div>
+                <input 
+                type="number" 
+                value={number} 
+                onChange={e => setNumber(e.target.value)} />
+            </div>
+            <SectionTitle title="Exercicio #02"/>
+
         </div>
     )
 }
