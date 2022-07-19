caef87c94fef729d8a650761c382c9738699bffa
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-19 12:51:59 -0300

    s5a68: useRef #02

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -2,41 +2,63 @@ import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 const UseRef = (props) => {
-    const [value1, setValue1] = useState("")
-    const count = useRef(0);
-    useEffect(function(){
+    const [value1, setValue1] = useState("");
+    const [value2, setValue2] = useState("");
 
+    const count = useRef(0);
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
+
+
+    useEffect(function(){
         count.current = count.current + 1
+        myInput2.current.focus()
     }, [value1])
+
+    useEffect(function(){
+        count.current = count.current + 1
+        myInput1.current.focus()
+    }, [ value2])
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
-        <SectionTitle title="Exercicio #01"/>
-        <div className="center">
-            <div>
-                <span className="text">
-                    valor:
-                </span>
-                <span className="text">
-                    {value1} [
-                </span>
-                <span className="text red">
-                    {count.current}
-                </span>
-                <span className="text">
-                    ]
-                </span>
+            <SectionTitle title="Exercicio #01"/>
+            <div className="center">
+                <div>
+                    <span className="text">
+                        valor:
+                    </span>
+                    <span className="text">
+                        {value1 + value2} [
+                    </span>
+                    <span className="text red">
+                        {count.current}
+                    </span>
+                    <span className="text">
+                        ]
+                    </span>
+                </div>
+                <input 
+                type="text"
+                ref={myInput1}
+                className="input" 
+                value={value1}
+                onChange={e => setValue1(e.target.value)}
+                />
             </div>
-            <input 
-            type="text"
-            className="input" 
-            value={value1}
-            onChange={e => setValue1(e.target.value)}
-            />
-        </div>
+            <SectionTitle title="Exercicio #02"/>
+            <div className="center">
+                <input 
+                type="text" 
+                ref={myInput2}
+                className="input"
+                value={value2}
+                onChange={e => setValue2(e.target.value)}
+                />
+            </div>
         </div>
     )   
 }
