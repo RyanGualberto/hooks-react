278c82aa38c9e98b609f89fa3a096ff155421ce8
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-20 09:14:21 -0300

    s5a75: useReducer #01

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,13 +1,56 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+const initialState = {
+    number: 0,
+    cart: [],
+    user: null,
+    products: [{}, {}]
+}
+
+function reducer(state, action){
+    switch (action.type) {
+        case 'add2ToNumber':
+            return {...state, number: state.number + 2}
+        case 'login':
+            return {...state, user: {name: action.name}}
+        default:
+            return state
+            break;
+    }
+}
+
 const UseReducer = (props) => {
+    const [state, exec] = useReducer(reducer, initialState)
+
     return (
         <div className="UseReducer">
             <PageTitle
                 title="Hook UseReducer"
                 subtitle="Uma outra forma de ter estado em componentes funcionais!"
             />
+
+            <div className="center">
+                {state.user ? 
+                    <span className="text">{state.user.name}</span>
+                    :  <span className="text">Sem usuario conectado</span>
+                }
+                <span className="text">{state.number}</span>
+                <div>
+                    <button 
+                    className='btn'
+                    onClick={() => exec({type: 'add2ToNumber'})}
+                    >
+                        +2
+                    </button>
+                    <button 
+                    className='btn'
+                    onClick={() => exec({type: 'login', name: 'Ryan'})}
+                    >
+                        login
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
